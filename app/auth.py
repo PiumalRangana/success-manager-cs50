09e88b4f681148473307914b75ac692cd97f1d68
PiumalRangana <piumalrangana4@gmail.com>
2025-11-03 10:36:09 +0530

    Implement user login feature with route, authentication logic, and login form template

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from  .models import User
 from . import db
 
@@ -56,3 +56,44 @@ def register():
         return redirect("/login")
     
     return render_template("register.html")
+
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        errors = []
+
+        # ===== Validations =====
+        # 1. Required fields
+        if not email or not password:
+            errors.append("All fields are required.")
+
+        # 2. Email format check
+        if "@" not in email or "." not in email:
+            errors.append("Invalid email address.")
+
+        # ===== Handle errors =====
+        if errors:
+            return render_template("login.html", errors=errors, email=email)
+        
+        # ====== Authenticate User =====
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            errors.append("Invalid email.")
+        
+        if errors:
+            return render_template("login.html", errors=errors, email=email)
+        
+        # =====Check password=====
+        if not check_password_hash(existing_user.password_hash, password):
+            errors.append("Invalid password.")
+
+        if errors:
+            return render_template("login.html", errors=errors, email=email)
+        
+        return render_template("index.html")
+
+    return render_template("login.html")
