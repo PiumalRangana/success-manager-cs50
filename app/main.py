cadf690c143e6edf4371b336b81bc73a9bf123a5
PiumalRangana <piumalrangana4@gmail.com>
2025-12-01 09:59:11 +0530

    featur: add task feature is added to the front end. server receves data and prints in the terminal successfully.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
@@ -20,7 +20,13 @@ def login():
     return render_template("login.html")
 
 @login_required
-@main.route('/home')
+@main.route('/home',methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        task_txt = request.form.get("task_txt")
+        is_daily = request.form.get("is_daily")
+        task_color = request.form.get("tsk_color")
+        print(task_txt,is_daily,task_color)
+
     user = {'user_id': current_user.id, 'user_name': current_user.name}
     return render_template('index.html', user=user)
