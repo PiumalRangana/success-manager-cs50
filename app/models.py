e08198bb5acc949a012db98b932884648dd96efc
PiumalRangana <piumalrangana4@gmail.com>
2025-12-19 21:53:22 +0530

    Aded is_deleted column to the model. need it for soft delete

--- app/models.py
+++ app/models.py
@@ -11,13 +11,16 @@ class User(UserMixin, db.Model):
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('tasks', lazy=True))
+
     task_text = db.Column(db.String(256), nullable=False)
     time_spent = db.Column(db.Integer, default=0)
     is_complete = db.Column(db.Boolean, default=False, nullable=False)
     is_daily = db.Column(db.Boolean, default=False, nullable=False)
     color = db.Column(db.String(50))
+    is_deleted = db.Column(db.Boolean, default=False, nullable=False)
 
 class TimeSession(db.Model):
     id = db.Column(db.Integer, primary_key=True)
