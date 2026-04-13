74e722e533da00981e8ee66cb3fddad624636a34
PiumalRangana <piumalrangana4@gmail.com>
2026-04-13 07:53:57 +0530

    feat: add real-time legend with session aggregation and live updates
    - add interval loop for live tracking of active sessions

--- app/static/dailyTimeRing/legand/index.js
+++ app/static/dailyTimeRing/legand/index.js
@@ -3,12 +3,35 @@ import { getLegendData } from "./legendPipeline.js";
 import { getTodaySessions, subscribe } from "../dailySessionStore.js";
 
 export function initLegend(){
-    let todaySessions = getTodaySessions();
-    const legendData = getLegendData(todaySessions);
-    createLegendRenderer("#legend", legendData);
+    let initialSessions = getTodaySessions();
+    convertDataAndRender(initialSessions);
 
     subscribe((sessions) => {
+        convertDataAndRender(sessions);
+    });
+
+    startLegendLoop();
+}
+
+let intervalId = null;
+
+function startLegendLoop() {
+    if (intervalId) return;
+
+    intervalId = setInterval(() => {
+        const sessions = getTodaySessions();
+        convertDataAndRender(sessions);
+    }, 1000);
+}
+
+function stopLegendLoop() {
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+}
+
+function convertDataAndRender(sessions) {
         const legendData = getLegendData(sessions);
         createLegendRenderer("#legend", legendData);
-    });
 }
